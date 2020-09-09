30e7d2a0d5ef1e8d4b5044c48e2d121630753029
mishikag <23mgupta@athenian.org>
2020-09-09 20:10:51 +0000

    finished

--- my_code.py
+++ my_code.py
@@ -2,3 +2,8 @@
 #  
 
 # Write a program that asks for the user's name and another piece of information.Then prints a response using both of the inputs.
+
+name = input("What is your name?")
+age = input("How old are you?")
+
+print("your name is", name, "and you are", age, "years old")
